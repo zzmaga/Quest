93325827b3542f6470a802c1c59307a93e9f29e0
mtastan <magjantastanov@gmail.com>
2024-12-03 22:43:27 +0500

    comm

--- checkpoint.go
+++ checkpoint.go
@@ -1,5 +1,12 @@
 package piscine
 
+/*
+	Write a function DigitLen() that takes two integers as arguments and
+
+returns the times the first int can be divided by the second until it reaches zero.
+The second int must be between 2 and 36. If no, the function returns -1.
+- If the first int is negative, reverse the sign and count the digits.
+*/
 func DigitLen(n, base int) int {
 	if base < 2 || base > 36 {
 		return -1
